93caedc3784fac3b12d3bbde437dc21ea57ff024
Bhavin Borkhataria <bhavin1234@gmail.com>
2024-02-03 14:58:50 +0300

    removed logo from home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 // import React from 'react';
-import logo from '../assets/react.svg';
+// import logo from '../assets/react.svg';
 // import './App.css';
 
 function Home() {
   return (
     <div className="App">
                 <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                {/* <img src={logo} className="App-logo" alt="logo" /> */}
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
